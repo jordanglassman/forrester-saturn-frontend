5c897bdbb40b3afcd3426acdd61ea0f0722a9c48
Michael Gagliardo <mgagliardo91@gmail.com>
2018-06-28 10:20:55 -0400

    ANALYTICS-2007 New features

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -9,7 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Goodbye World!" );
-        System.out.println( "Goodbye All!" );
+        System.out.println( "Goodbye All!" ););
+        System.out.println( "Goodbye Nobody!" );
         // insert bad code here
     }
 }
