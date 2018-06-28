e5a1436adbef4fc7b6936d1253c818530e79d0fc
Michael Gagliardo <mgagliardo91@gmail.com>
2018-06-28 10:07:07 -0400

    ANALYTICS-2002 Goodbye All

--- src/main/java/loki/App.java
+++ src/main/java/loki/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Goodbye World!" );
+        System.out.println( "Goodbye All!" );
         // insert bad code here
     }
 }
